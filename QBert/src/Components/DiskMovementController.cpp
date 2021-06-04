9d9276f4fedc4e36e7a36058f1007c17636dd784
Milan Dierick <milan.dierick@gmail.com>
2021-06-04 04:40:32 +0200

    Added movement controller for disks

---
+++ QBert/src/Components/DiskMovementController.cpp
@@ -0,0 +1,108 @@
+﻿#include "DiskMovementController.h"
+
+#include "QBertMovementController.h"
+#include "SandboxScene.h"
+
+DiskMovementController::DiskMovementController(const Hex currentHex,
+											   const Layout hexagonalGridLayout,
+											   const size_t ticksBetweenMoves,
+											   const size_t ticksSinceLastMove,
+											   const size_t ticksPerMove,
+											   const Heirloom::Ref<std::unordered_set<Hex>> hexagons,
+											   Heirloom::GameObject* const parent)
+	: m_CurrentHex(currentHex),
+	  m_TargetHex(Hex(static_cast<int>(SandboxScene::Configuration.InitialQBertPosition.x - 1),
+					  static_cast<int>(SandboxScene::Configuration.InitialQBertPosition.y + 2),
+					  -static_cast<int>(SandboxScene::Configuration.InitialQBertPosition.z))),
+	  m_HexagonalGridLayout(hexagonalGridLayout),
+	  m_TicksBetweenMoves(ticksBetweenMoves),
+	  m_TicksSinceLastMove(ticksSinceLastMove),
+	  m_TicksPerMove(ticksPerMove),
+	  m_DistanceAlreadyMoved(0.0f),
+	  m_CurrentState(DiskMovementState::Idle),
+	  m_Hexagons(hexagons),
+	  m_Parent(parent) { UpdateTransformPosition(); }
+
+DiskMovementController::~DiskMovementController()
+{
+	HL_TRACE("DiskMovementController being destroyed!");
+}
+
+void DiskMovementController::Update(Heirloom::Timestep ts)
+{
+	UNREFERENCED_PARAMETER(ts);
+
+	if (m_QBertMovementController == nullptr) return;
+	if (m_QBertMovementController->GetCurrentHex().Q != m_CurrentHex.Q && m_QBertMovementController->GetCurrentHex().R
+		!= m_CurrentHex.R) { return; }
+
+	switch (m_CurrentState)
+	{
+		case DiskMovementState::Idle: StartMovingDisk();
+			break;
+		case DiskMovementState::Moving: MoveDisk();
+			break;
+		case DiskMovementState::Destination: FinishedMovingDisk();
+			break;
+	}
+}
+
+void DiskMovementController::Render()
+{
+}
+
+void DiskMovementController::StartMovingDisk()
+{
+	m_CurrentState = DiskMovementState::Moving;
+	m_QBertMovementController->SetCurrentState(QBertMovementState::Disk);
+	MoveDisk();
+}
+
+void DiskMovementController::MoveDisk() { MoveTowardsTargetHex(m_TicksPerMove); }
+
+void DiskMovementController::FinishedMovingDisk()
+{
+	Hex dropOffHex = HexNeighbor(m_CurrentHex, 2);
+	dropOffHex     = HexNeighbor(dropOffHex, 1);
+
+	m_CurrentState = DiskMovementState::Destination;
+	m_QBertMovementController->SetCurrentState(QBertMovementState::Moving);
+
+	m_QBertMovementController->SetCurrentHex(m_CurrentHex);
+	m_QBertMovementController->SetTargetHex(dropOffHex);
+
+	m_Parent->SetActive(false);
+}
+
+void DiskMovementController::UpdateTransformPosition() const
+{
+	const glm::vec3 alignedPosition = {HexagonalGrid::HexToPixel(m_HexagonalGridLayout, m_CurrentHex), 1.0f};
+	m_Parent->GetTransform()->SetPosition(alignedPosition);
+}
+
+void DiskMovementController::MoveTowardsTargetHex(const size_t totalTicksForMove)
+{
+	const glm::vec3 currentHexPosition = {HexagonalGrid::HexToPixel(m_HexagonalGridLayout, m_CurrentHex), 0.0f};
+	const glm::vec3 targetHexPosition  = {HexagonalGrid::HexToPixel(m_HexagonalGridLayout, m_TargetHex), 0.0f};
+	glm::vec3 currentPosition          = m_Parent->GetTransform()->GetPosition();
+
+	const glm::vec3 totalDistanceToMove = targetHexPosition - currentHexPosition;
+
+	if (std::abs(m_DistanceAlreadyMoved.x) >= std::abs(totalDistanceToMove.x) && std::abs(m_DistanceAlreadyMoved.y) >=
+		std::abs(totalDistanceToMove.y))
+	{
+		m_CurrentHex           = m_TargetHex;
+		m_DistanceAlreadyMoved = glm::vec3(0.0f);
+		UpdateTransformPosition();
+		FinishedMovingDisk();
+	}
+
+	currentPosition.x += totalDistanceToMove.x / totalTicksForMove;
+	currentPosition.y += totalDistanceToMove.y / totalTicksForMove;
+
+	m_DistanceAlreadyMoved.x += totalDistanceToMove.x / totalTicksForMove;
+	m_DistanceAlreadyMoved.y += totalDistanceToMove.y / totalTicksForMove;
+
+	m_Parent->GetTransform()->SetPosition(currentPosition);
+	m_QBertMovementController->GetParent()->GetTransform()->SetPosition(currentPosition);
+}
---
+++ QBert/src/Components/DiskMovementController.h
@@ -0,0 +1,67 @@
+﻿// Author: Milan Dierick
+// Created: 03/06/2021 11:35 PM
+// Solution: QBert
+
+#pragma once
+#include <unordered_set>
+
+#include "Heirloom.h"
+#include "HexagonalGrid/Hexagon.h"
+#include "HexagonalGrid/HexagonalGrid.h"
+#include "HexagonalGrid/Layout.h"
+
+class QBertMovementController;
+
+enum class DiskMovementState
+{
+	Idle,
+	Moving,
+	Destination
+};
+
+class DiskMovementController final : public Heirloom::Component
+{
+public:
+	[[nodiscard]] DiskMovementController(Hex currentHex,
+										 Layout hexagonalGridLayout,
+										 size_t ticksBetweenMoves,
+										 size_t ticksSinceLastMove,
+										 size_t ticksPerMove,
+										 Heirloom::Ref<std::unordered_set<Hex>> hexagons,
+										 Heirloom::GameObject* parent);
+
+	~DiskMovementController() override;
+
+	[[nodiscard]] Heirloom::GameObject* GetParent() const override { return m_Parent; }
+	void SetParent(Heirloom::GameObject* const parent) override { m_Parent = parent; }
+	[[nodiscard]] Hex GetCurrentHex() const { return m_CurrentHex; }
+
+	void SetQBertMovementController(QBertMovementController* qBertMovementController)
+	{
+		m_QBertMovementController = qBertMovementController;
+	}
+
+	void Update(Heirloom::Timestep ts) override;
+	void Render() override;
+
+private:
+	Hex m_CurrentHex;
+	Hex m_TargetHex;
+	Layout m_HexagonalGridLayout;
+	size_t m_TicksBetweenMoves;
+	size_t m_TicksSinceLastMove;
+	size_t m_TicksPerMove;
+	glm::vec3 m_DistanceAlreadyMoved;
+	DiskMovementState m_CurrentState;
+
+	QBertMovementController* m_QBertMovementController = nullptr;
+	Heirloom::Ref<std::unordered_set<Hex>> m_Hexagons;
+
+	Heirloom::GameObject* m_Parent;
+
+	void StartMovingDisk();
+	void MoveDisk();
+	void FinishedMovingDisk();
+	void UpdateTransformPosition() const;
+	void MoveTowardsTargetHex(size_t totalTicksForMove);
+};
